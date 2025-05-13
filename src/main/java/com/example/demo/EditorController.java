6600881e8e5f026559240e259918655534d522f4
tnguyen2030 <75008046+tnguyen2030@users.noreply.github.com>
2025-05-13 14:07:34 +0700

    update project

--- src/main/java/com/example/demo/EditorController.java
+++ src/main/java/com/example/demo/EditorController.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
 import java.io.File;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
@@ -20,10 +21,11 @@ public class EditorController {
     File jsonFile = new File("src\\main\\resources\\static\\PolicyPack_20250509.json");
     // Thêm vào model (truy cập được ở Thymeleaf như ${policy.policyNumber}, v.v.)
     try {
-      JsonNode root = mapper.readTree(jsonFile);
-      JsonNode policyNode = root.get("MBALPrintPolicyVO");
-      MBALPrintPolicyVO policyData = mapper.treeToValue(policyNode, MBALPrintPolicyVO.class);
-      model.addAttribute("MBALPrintPolicyVO", policyData);
+      TypeReference<Map<String, Object>> typeRef = new TypeReference<>() {
+      };
+      Map<String, Object> root = mapper.readValue(jsonFile, typeRef);
+      Map<String, Object> MBALPrintPolicyVO = (Map<String, Object>) root.get("MBALPrintPolicyVO");
+      model.addAttribute("MBALPrintPolicyVO", MBALPrintPolicyVO);
     } catch (Exception e) {
       e.printStackTrace();
       model.addAttribute("error", "Không thể đọc file JSON");
